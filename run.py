3a54968ecb5e53f4d1396ae26c7d54dea7274d6d
HYPERHITLER <h38463962@gmail.com>
2024-06-07 10:05:42 +0545

    Create run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+import os, platform
+print(' CHECKING FOR UPDATES')
+bit = platform.architecture()[0]
+if bit == '32':
+  from data import HYPER32
+elif bit == '64':
+  from data import HYPER64
+else: print(' YOUR DEVICE DOESN'T SUPPORT MY TOOL')
+  
