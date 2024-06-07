eaf91581d9b05ee2fde54f392310adba6a598fd1
HYPERHITLER <h38463962@gmail.com>
2024-06-07 10:13:40 +0545

    Update run.py

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ if bit == '32':
   from data import HYPER32
 elif bit == '64':
   from data import HYPER64
-else: print(' YOUR DEVICE DOESN'T SUPPORT MY TOOL')
+else: print(" YOUR DEVICE DOESN'T SUPPORT MY TOOL")
   
