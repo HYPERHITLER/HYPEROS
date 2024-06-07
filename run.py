ab9304440b7542a29488182e0545c83c8229b24d
HYPERHITLER <h38463962@gmail.com>
2024-06-07 10:15:30 +0545

    Update run.py

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import os, platform
 print(' CHECKING FOR UPDATES')
 os.system('git pull')
 bit = platform.architecture()[0]
-if bit == '32':
+if bit == '32bit':
     from data import HYPER32
-elif bit == '64':
+elif bit == '64bit':
     from data import HYPER64
 else: print(" YOUR DEVICE DOESN'T SUPPORT MY TOOL")
   
